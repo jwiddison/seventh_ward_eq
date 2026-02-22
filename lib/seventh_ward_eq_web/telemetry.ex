a7f8e105a878bdc75e37102e7a8e538ee4c92cf6
Jordan Widdison <jordan.widdison@gmail.com>
2026-02-22 11:36:34 -0700

    Add mix quality

--- lib/seventh_ward_eq_web/telemetry.ex
+++ lib/seventh_ward_eq_web/telemetry.ex
@@ -71,8 +71,7 @@ defmodule SeventhWardEqWeb.Telemetry do
       ),
       summary("seventh_ward_eq.repo.query.idle_time",
         unit: {:native, :millisecond},
-        description:
-          "The time the connection spent waiting before being checked out for the query"
+        description: "The time the connection spent waiting before being checked out for the query"
       ),
 
       # VM Metrics
